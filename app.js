e1a96165d1b23eebe1abd5cf1206361fd2f77edd
geoffrey184 <geoffreynono@icloud.com>
2024-03-11 16:53:09 +0100

    troisieme

--- app.js
+++ app.js
@@ -1,51 +1,39 @@
-// const nav = document.querySelector(".nav"),
-//   navOpenBtn = document.querySelector(".navOpenBtn"),
-//   navCloseBtn = document.querySelector(".navCloseBtn");
-
-
-// navOpenBtn.addEventListener("click", () => {
-//   nav.classList.add("openNav");
-//   nav.classList.remove("openSearch");
-//   searchIcon.classList.replace("uil-times", "uil-search");
-// });
-// navCloseBtn.addEventListener("click", () => {
-//   nav.classList.remove("openNav");
-// });
-
 const body = document.querySelector("body"),
-      nav = document.querySelector("nav"),
-      modeToggle = document.querySelector(".dark-light"),
-      sidebarOpen = document.querySelector(".sidebarOpen"),
-      siderbarClose = document.querySelector(".siderbarClose");
+  nav = document.querySelector("nav"),
+  modeToggle = document.querySelector(".dark-light"),
+  sidebarOpen = document.querySelector(".sidebarOpen"),
+  siderbarClose = document.querySelector(".siderbarClose");
 
-      let getMode = localStorage.getItem("mode");
-          if(getMode && getMode === "dark-mode"){
-            body.classList.add("dark");
-          }
+let getMode = localStorage.getItem("mode");
+if (getMode && getMode === "dark-mode") {
+  body.classList.add("dark");
+}
 
 // js code to toggle dark and light mode
-      modeToggle.addEventListener("click" , () =>{
-        modeToggle.classList.toggle("active");
-        body.classList.toggle("dark");
+modeToggle.addEventListener("click", () => {
+  modeToggle.classList.toggle("active");
+  body.classList.toggle("dark");
 
-        // js code to keep user selected mode even page refresh or file reopen
-        if(!body.classList.contains("dark")){
-            localStorage.setItem("mode" , "light-mode");
-        }else{
-            localStorage.setItem("mode" , "dark-mode");
-        }
-      });
- 
-      
-//   js code to toggle sidebar
-sidebarOpen.addEventListener("click" , () =>{
-    nav.classList.add("active");
+  // js code to keep user selected mode even page refresh or file reopen
+  if (!body.classList.contains("dark")) {
+    localStorage.setItem("mode", "light-mode");
+  } else {
+    localStorage.setItem("mode", "dark-mode");
+  }
 });
 
-body.addEventListener("click" , e =>{
-    let clickedElm = e.target;
+//   js code to toggle sidebar
+sidebarOpen.addEventListener("click", () => {
+  nav.classList.add("active");
+});
 
-    if(!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")){
-        nav.classList.remove("active");
-    }
+body.addEventListener("click", (e) => {
+  let clickedElm = e.target;
+
+  if (
+    !clickedElm.classList.contains("sidebarOpen") &&
+    !clickedElm.classList.contains("menu")
+  ) {
+    nav.classList.remove("active");
+  }
 });
